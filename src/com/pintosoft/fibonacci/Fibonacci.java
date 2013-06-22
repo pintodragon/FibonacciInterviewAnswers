8a940d77c3eda261d515586921cd1c2290d4bd13
Justin Chrysler <pintodragon@gmail.com>
2013-06-22 17:44:06 -0400

    Initial commit

---
+++ src/com/pintosoft/fibonacci/Fibonacci.java
@@ -0,0 +1,5 @@
+package com.pintosoft.fibonacci;
+
+public interface Fibonacci {
+	public int fibonacci(int num);
+}
---
+++ src/com/pintosoft/fibonacci/RecursiveFibonacci.java
@@ -0,0 +1,20 @@
+package com.pintosoft.fibonacci;
+
+public class RecursiveFibonacci implements Fibonacci {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int fibonacci(int num) {
+		
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
