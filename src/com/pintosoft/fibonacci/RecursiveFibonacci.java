cacb71e09e86fc8676214e77dfba2f0eb9c87af5
Justin Chrysler <pintodragon@gmail.com>
2013-06-22 18:07:39 -0400

    A working recursive version of Fibonacci numbers

--- src/com/pintosoft/fibonacci/RecursiveFibonacci.java
+++ src/com/pintosoft/fibonacci/RecursiveFibonacci.java
@@ -6,15 +6,32 @@ public class RecursiveFibonacci implements Fibonacci {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RecursiveFibonacci recFib = new RecursiveFibonacci();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 30; i++) {
+			if (i == 0) {
+				sb.append(recFib.fibonacci(i));
+			} else {
+				sb.append(", " + recFib.fibonacci(i));
+			}
+		}
+		
+		System.out.println(sb.toString());
 	}
 
 	@Override
 	public int fibonacci(int num) {
+		int fibNum = 0;
 		
-		// TODO Auto-generated method stub
-		return 0;
+		if (num > 0) {
+			if (num < 2) {
+				fibNum = num;
+			} else {
+				fibNum = fibonacci(num-1) + fibonacci(num-2);
+			}
+		}
+		
+		return fibNum;
 	}
 
 }
